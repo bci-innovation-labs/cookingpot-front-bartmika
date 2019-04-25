7eb7ac63bdd171a36e43ff643976ad0f0be23167
unknown <bmika@icloud.com>
2019-04-25 17:05:57 -0400

    Added GUI.

--- src/Components/FoodDetailComponent.js
+++ src/Components/FoodDetailComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class FoodDetailComponent extends Component {
     render() {
@@ -7,6 +8,20 @@ class FoodDetailComponent extends Component {
             <div className="container-fluid">
                 <div className="d-flex align-items-stretch">
                     <main id="main" role="main">
+
+                        <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                          <li className="breadcrumb-item">
+                              <a href="/dashboard">
+                                  <i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard
+                              </a>
+                           </li>
+                          <li className="breadcrumb-item active" aria-current="page">
+                              Food Details
+                           </li>
+                        </ol>
+                      </nav>
+
                         <h1>Food Details</h1>
 
                         <div className="row mt-4 pt-3 mb-4 pb-2">
@@ -14,15 +29,29 @@ class FoodDetailComponent extends Component {
                                 <table className="table table-bordered custom-cell-w">
                                     <tbody>
                                     <tr className="bg-dark">
-                                        <th scope="row" colSpan="2" className="text-light">Personal details</th>
+                                        <th scope="row" colSpan="2" className="text-light">Food details</th>
                                     </tr>
                                     <tr>
                                         <th scope="row" className="bg-light">Name</th>
                                         <td>{foodDetail.name}</td>
                                     </tr>
 
+                                    <tr>
+                                        <th scope="row" className="bg-light">Description</th>
+                                        <td>{foodDetail.longDescription}</td>
+                                    </tr>
+
                                     </tbody>
                                 </table>
+
+                                <div className="form-group col-md-12 mb-3 mx-auto text-center">
+                                    <Link to="/dashboard" className="btn btn-success btn-lg btn-fxw mt-4">
+                                    <i className="fas fa-arrow-left"></i>&nbsp;
+                                    Back to Dashboard
+                                    </Link>
+                                </div>
+
+
                             </div>
                         </div>
 
